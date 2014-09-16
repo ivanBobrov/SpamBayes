ebd8352d01d71fd4365dedd764dc1f6bad480d5b
imponderabilidad <imponderabilidad@yandex.ru>
2014-09-16 07:53:31 +0400

    loading to instances

--- src/com/antiSpam/spamBayes/SpamBayes.java
+++ src/com/antiSpam/spamBayes/SpamBayes.java
@@ -1,16 +1,29 @@
 package com.antiSpam.spamBayes;
 
 
-import weka.core.Instances;
+import com.sun.xml.internal.stream.events.CharacterEvent;
+import sun.security.jca.GetInstance;
+import weka.core.*;
 import weka.classifiers.bayes.NaiveBayes;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SpamBayes {
+    private static HashMap<String, Integer> indexMap = new HashMap<String, Integer>();
+
     public static void main(String argv[]) {
+        tryoutsmth(argv);
+    }
+
+
+    public static void readArff() {
         Instances data = null;
         Instances unlabeled = null;
         try {
@@ -42,6 +55,123 @@ public class SpamBayes {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        System.out.print("done");
     }
+
+    public static void tryoutsmth(String argv[]) {
+        String filename = argv[0];
+        System.out.println(filename);
+
+        Instances instances = new Instances("Relation", new FastVector(), 5000000);
+        InputStream input = null;
+        try {
+            input = new FileInputStream(filename);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error input stream creation");
+        }
+
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        try {
+            int currentLine = 0;
+            XMLEventReader r = factory.createXMLEventReader(input);
+            while (r.hasNext() && currentLine < 210) {
+                XMLEvent event = r.nextEvent();
+                if (event.isStartElement() && event.asStartElement().getName().toString().equals("text")) {
+                    XMLEvent text = r.nextEvent();
+                    addString(((CharacterEvent) text).getData().replace("\n", " "), instances);
+                    System.out.print("\r " + currentLine++);
+                }
+            }
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+            System.out.println("Something wrong :)");
+        }
+
+        replaceAllMissing(instances);
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("result.arff"));
+            writer.write(instances.toString());
+            writer.newLine();
+            writer.flush();
+            writer.close();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static void replaceAllMissing(Instances instances) {
+        for (int i = 0; i < instances.numInstances(); i++) {
+            Instance instance = instances.instance(i);
+            for (int j = 0; j < instance.numAttributes(); j++) {
+                if (instance.isMissing(j)) {
+                    instance.setValue(j, 0);
+                }
+            }
+        }
+    }
+
+    public static void addString(String string, Instances instances) {
+        ArrayList<String> bases = splitString(string);
+
+        SparseInstance newInstance = new SparseInstance(instances.numAttributes());
+        /*for (int i = 0; i < newInstance.numAttributes(); i++) {
+            newInstance.setValue(i, 0);
+        }*/
+
+        for (String base : bases) {
+            Integer index = indexMap.get(base);
+            if (index == null) {
+                index = indexMap.size();
+                indexMap.put(base, index);
+
+                Attribute attribute = new Attribute(base);
+                instances.insertAttributeAt(attribute, index);
+                newInstance.insertAttributeAt(index);
+                newInstance.setValue(index, 1);
+            } else {
+                if (newInstance.isMissing(index)) {
+                    newInstance.setValue(index, 1);
+                } else {
+                    newInstance.setValue(index, newInstance.value(index) + 1);
+                }
+            }
+
+        }
+
+        instances.add(newInstance);
+    }
+
+    public static ArrayList<Integer> parseString(String string) {
+        ArrayList<Integer> baseIndices = new ArrayList<Integer>();
+        ArrayList<String> bases = splitString(string);
+
+        for (String base : bases) {
+            Integer index = indexMap.get(base);
+            if (index == null) {
+                int newIdx = indexMap.size();
+                indexMap.put(base, newIdx);
+                baseIndices.add(newIdx);
+            } else {
+                baseIndices.add(index);
+            }
+        }
+
+        return baseIndices;
+    }
+
+    public static ArrayList<String> splitString(String string) {
+        int nGramLength = 3;
+        ArrayList<String> bases = new ArrayList<String>();
+
+        if (string.length() > nGramLength) {
+            for (int i = 0; i < string.length() - (nGramLength - 1); i++) {
+                bases.add(string.substring(i, i + nGramLength));
+            }
+        }
+
+        return bases;
+    }
+
+
 }
