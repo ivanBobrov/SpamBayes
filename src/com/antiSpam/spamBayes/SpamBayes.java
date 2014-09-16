73f19823f9e55f92e7bfb1360322c70d29dea3b1
Bobrov Ivan <id.bobr@gmail.com>
2014-09-16 17:36:46 +0400

    massive refactoring

--- src/com/antiSpam/spamBayes/SpamBayes.java
+++ src/com/antiSpam/spamBayes/SpamBayes.java
@@ -2,7 +2,6 @@ package com.antiSpam.spamBayes;
 
 
 import com.sun.xml.internal.stream.events.CharacterEvent;
-import sun.security.jca.GetInstance;
 import weka.core.*;
 import weka.classifiers.bayes.NaiveBayes;
 
@@ -18,6 +17,164 @@ public class SpamBayes {
     private static HashMap<String, Integer> indexMap = new HashMap<String, Integer>();
 
     public static void main(String argv[]) {
+        String outputFilename = "result.arff";
+        String hamFilename = argv[0];
+        String spamFilename = argv[1];
+        String testFilename = argv[2];
+
+        Instances trainingContent = getNewContentInstances();
+        loadContent(trainingContent, hamFilename, 0, 50, 0, true);
+        loadContent(trainingContent, spamFilename, 0, 50, 1, true);
+
+        //It makes attributes of training and test contents match
+        Instances testContent = new Instances(trainingContent);
+        testContent.delete();
+
+        loadContent(testContent, testFilename, 0, 50, 0, false);
+
+        classifyTestContent(trainingContent, testContent);
+
+        replaceAllMissing(testContent);
+        saveContenToFile(testContent, outputFilename);
+    }
+
+    private static void saveContenToFile(Instances instances, String filename) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            try {
+                writer.write(instances.toString());
+                writer.newLine();
+                writer.flush();
+            } catch (IOException exception) {
+                System.out.println("Saving error");
+            } finally {
+                writer.close();
+            }
+        } catch (IOException exception) {
+            System.out.println("Opening output stream error");
+            exception.printStackTrace();
+        }
+    }
+
+    private static void classifyTestContent(Instances trainingSet, Instances testSet) {
+        trainingSet.setClassIndex(trainingSet.numAttributes() - 1);
+        testSet.setClassIndex(testSet.numAttributes() - 1);
+
+        NaiveBayes bayes = new NaiveBayes();
+
+        try {
+            bayes.buildClassifier(trainingSet);
+            for (int i = 0; i < testSet.numInstances(); i++) {
+                double clsLabel = bayes.classifyInstance(testSet.instance(i));
+                testSet.instance(i).setClassValue(clsLabel);
+            }
+        } catch (Exception e) {
+            System.out.println("Classification error");
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    loadSize is the number of messages to load from file. Zero value means all data.
+    classType is the predefined class of all these messages. Zero - legal message, One - spam.
+     */
+    private static void loadContent(Instances instances,
+                                    String filename,
+                                    int fromMessage, // that was loadSize
+                                    int toMessage,
+                                    double classType,
+                                    boolean addNewAttributes) {
+        System.out.println("Loading " + filename);
+
+        try {
+            FileInputStream inputStream = new FileInputStream(filename);
+            try {
+                XMLEventReader xmlEventReader = XMLInputFactory.newInstance().createXMLEventReader(inputStream);
+
+                int currentLine = 0;
+                while (xmlEventReader.hasNext()) {
+                    XMLEvent event = xmlEventReader.nextEvent();
+
+                    if (event.isStartElement() && event.asStartElement().getName().toString().equals("text")) {
+                        XMLEvent textEvent = xmlEventReader.nextEvent();
+
+                        if (currentLine < fromMessage) {
+                            continue;
+                        }
+
+                        String text = ((CharacterEvent) textEvent).getData().replace("\n", " ").toLowerCase();
+                        addStringToData(instances, text, classType, addNewAttributes);
+                        System.out.print("\r" + currentLine++);
+
+                        if (toMessage != 0 && currentLine > toMessage) {
+                            break;
+                        }
+                    }
+                }
+                System.out.print("\n");
+
+            } catch (XMLStreamException exception) {
+                System.out.println("Error parsing " + filename);
+                exception.printStackTrace();
+            } finally {
+                try {
+                    inputStream.close();
+                } catch (IOException exception) {
+                    System.out.println("Can't close stream. Exit.");
+                    System.exit(1);
+                }
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println("File " + filename + " not found. Exit.");
+            System.exit(1);
+        }
+    }
+
+    private static void addStringToData(Instances instances, String text, double classType, boolean addNewAttributes) {
+        ArrayList<String> bases = splitString(text);
+        SparseInstance newInstance = new SparseInstance(instances.numAttributes());
+
+        for (String base : bases) {
+            Integer index = indexMap.get(base);
+            if (index == null) {
+                if (addNewAttributes) {
+                    index = indexMap.size();
+                    indexMap.put(base, index);
+
+                    Attribute attribute = new Attribute(base);
+                    instances.insertAttributeAt(attribute, index);
+                    newInstance.insertAttributeAt(index);
+                    newInstance.setValue(index, 1);
+                }
+            } else {
+                if (newInstance.isMissing(index)) {
+                    newInstance.setValue(index, 1);
+                } else {
+                    newInstance.setValue(index, newInstance.value(index) + 1);
+                }
+            }
+
+        }
+
+        newInstance.setValue(newInstance.numAttributes() - 1, classType);
+        instances.add(newInstance);
+    }
+
+    private static Instances getNewContentInstances() {
+        //This big number is capacity for instances.
+        Instances instances = new Instances("dataContent", new FastVector(), 1000000);
+
+        FastVector fastVector = new FastVector(2);
+        fastVector.addElement("ham");
+        fastVector.addElement("spam");
+        Attribute attribute = new Attribute("@@class@@", fastVector);
+
+        instances.insertAttributeAt(attribute, 0);
+
+        return instances;
+    }
+
+    public static void err(String argv[]) {
         tryoutsmth(argv);
         //readArff();
     }
@@ -70,7 +227,6 @@ public class SpamBayes {
         fastVector.addElement("spam");
         Attribute attribute = new Attribute("@@class@@", fastVector);
         instances.insertAttributeAt(attribute, 0);
-
 
         InputStream input = null;
 
