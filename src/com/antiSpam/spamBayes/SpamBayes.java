82a75e559128ebd1af98270b1b60c9e456fe0d70
Bobrov Ivan <id.bobr@gmail.com>
2014-09-16 16:16:22 +0400

    testing

--- src/com/antiSpam/spamBayes/SpamBayes.java
+++ src/com/antiSpam/spamBayes/SpamBayes.java
@@ -11,7 +11,6 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -20,6 +19,7 @@ public class SpamBayes {
 
     public static void main(String argv[]) {
         tryoutsmth(argv);
+        //readArff();
     }
 
 
@@ -44,7 +44,7 @@ public class SpamBayes {
 
 
         data.setClassIndex(0);
-        unlabeled.setClassIndex(0);
+        unlabeled.setClassIndex(unlabeled.numAttributes() - 1);
         NaiveBayes bayes = new NaiveBayes();
         try {
             bayes.buildClassifier(data);
@@ -60,34 +60,71 @@ public class SpamBayes {
 
     public static void tryoutsmth(String argv[]) {
         String filename = argv[0];
-        System.out.println(filename);
+        String goodFilename = argv[1];
+        String testFilename = argv[2];
+
 
         Instances instances = new Instances("Relation", new FastVector(), 5000000);
+        FastVector fastVector = new FastVector(2);
+        fastVector.addElement("ham");
+        fastVector.addElement("spam");
+        Attribute attribute = new Attribute("@@class@@", fastVector);
+        instances.insertAttributeAt(attribute, 0);
+
+
         InputStream input = null;
+
+        System.out.println(filename);
         try {
             input = new FileInputStream(filename);
         } catch (FileNotFoundException e) {
             System.out.println("Error input stream creation");
         }
 
-        XMLInputFactory factory = XMLInputFactory.newInstance();
+        read(instances, input, 1, 50, true);
+
+        System.out.println(goodFilename);
         try {
-            int currentLine = 0;
-            XMLEventReader r = factory.createXMLEventReader(input);
-            while (r.hasNext() && currentLine < 210) {
-                XMLEvent event = r.nextEvent();
-                if (event.isStartElement() && event.asStartElement().getName().toString().equals("text")) {
-                    XMLEvent text = r.nextEvent();
-                    addString(((CharacterEvent) text).getData().replace("\n", " "), instances);
-                    System.out.print("\r " + currentLine++);
-                }
-            }
-        } catch (XMLStreamException e) {
+            input = new FileInputStream(goodFilename);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-            System.out.println("Something wrong :)");
         }
 
+        read(instances, input, 0, 50, true);
+
+        System.out.println("replacing all missing");
         replaceAllMissing(instances);
+
+        Instances testInstances = new Instances(instances);
+        testInstances.delete();
+
+        System.out.println(testFilename);
+        try {
+            input = new FileInputStream(testFilename);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        read(testInstances, input, 0, 400, false);
+
+        //testInstances.
+
+        instances.setClassIndex(instances.numAttributes() - 1);
+        testInstances.setClassIndex(testInstances.numAttributes() - 1);
+
+        NaiveBayes bayes = new NaiveBayes();
+        try {
+            bayes.buildClassifier(instances);
+            for (int i = 0; i < testInstances.numInstances(); i++) {
+                double clsLabel = bayes.classifyInstance(testInstances.instance(i));
+                testInstances.instance(i).setClassValue(clsLabel);
+                System.out.println(i + "\t" + clsLabel);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
 
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("result.arff"));
@@ -98,6 +135,32 @@ public class SpamBayes {
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+
+        System.out.print("done");
+    }
+
+    private static void read(Instances instances, InputStream input, double clazz, int count, boolean rewrite) {
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        try {
+            int currentLine = 0;
+            XMLEventReader r = factory.createXMLEventReader(input);
+            while (r.hasNext()) {
+                XMLEvent event = r.nextEvent();
+                if (event.isStartElement() && event.asStartElement().getName().toString().equals("text")) {
+                    XMLEvent text = r.nextEvent();
+                    addString(((CharacterEvent) text).getData().replace("\n", " "), instances, clazz, rewrite);
+                    System.out.print("\r " + currentLine++);
+
+                    if (count != 0 && currentLine > count) {
+                        break;
+                    }
+                }
+            }
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+            System.out.println("Something wrong :)");
+        }
+        System.out.println("\n");
     }
 
     private static void replaceAllMissing(Instances instances) {
@@ -111,10 +174,11 @@ public class SpamBayes {
         }
     }
 
-    public static void addString(String string, Instances instances) {
+    public static void addString(String string, Instances instances, double clazz, boolean rewrite) {
         ArrayList<String> bases = splitString(string);
 
         SparseInstance newInstance = new SparseInstance(instances.numAttributes());
+
         /*for (int i = 0; i < newInstance.numAttributes(); i++) {
             newInstance.setValue(i, 0);
         }*/
@@ -122,13 +186,15 @@ public class SpamBayes {
         for (String base : bases) {
             Integer index = indexMap.get(base);
             if (index == null) {
-                index = indexMap.size();
-                indexMap.put(base, index);
+                if (rewrite) {
+                    index = indexMap.size();
+                    indexMap.put(base, index);
 
-                Attribute attribute = new Attribute(base);
-                instances.insertAttributeAt(attribute, index);
-                newInstance.insertAttributeAt(index);
-                newInstance.setValue(index, 1);
+                    Attribute attribute = new Attribute(base);
+                    instances.insertAttributeAt(attribute, index);
+                    newInstance.insertAttributeAt(index);
+                    newInstance.setValue(index, 1);
+                }
             } else {
                 if (newInstance.isMissing(index)) {
                     newInstance.setValue(index, 1);
@@ -139,6 +205,7 @@ public class SpamBayes {
 
         }
 
+        newInstance.setValue(newInstance.numAttributes() - 1, clazz);
         instances.add(newInstance);
     }
 
